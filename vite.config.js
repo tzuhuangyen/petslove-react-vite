88313d72918506216341848fa2cade73787fcb5f
tzuhuangyen <912yan@gmail.com>
2023-12-27 13:18:23 +0800

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/petslove-react-vite/",
+  base: process.env.NODE_ENV === "production" ? "/petslove-react-vite/" : "/",
   plugins: [react()],
 });
